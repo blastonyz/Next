180f543d88f8ef2a7064e61dde4cba5d37567be9
blastonyz <blastonyzamora@gmail.com>
2024-04-30 01:31:31 -0300

    .catch

--- app/components/products/ProductDetail.jsx
+++ app/components/products/ProductDetail.jsx
@@ -4,6 +4,37 @@ import QuantSelec from './QuantSelec';
 
 export const ProductDetail = async ({id}) => {
   console.log('detail id', id);
+    
+    const item = await fetch(`http://${process.env.VERCEL_URL}/api/producto/${id}`,{cache:"no-store"}).then(r => r.json()).catch(Error)
+    console.log('item detail',item);
+    
+  return (
+    <div className='max-w-4xl m-auto py-40 justify-center items-center'>
+        <section>
+           <Image
+            alt={item.title}
+            src={`/products-img/${item.image}`}
+            width={450}
+            height={450}
+            style={{objectFit: "contain"}}/>
+            <h2>{item.title}</h2>
+            <h2>$ {item.price}</h2>
+            <h2>Descripcion: {item.description}</h2>
+            <h2>Categoria: {item.category}</h2>
+            <h2>Stock: {item.stock}</h2>
+            <QuantSelec item={item}/>
+        </section>   
+    </div>
+  )
+}
+
+/*import React from 'react';
+import Image from 'next/image';
+import QuantSelec from './QuantSelec';
+
+export const ProductDetail = async ({id}) => {
+  console.log('detail id', id);
+    
     const item = await fetch(`http://${process.env.VERCEL_URL}/api/producto/${id}`,{cache:"no-store"}).then(r => r.json())
     console.log('item detail',item);
     
@@ -26,3 +57,44 @@ export const ProductDetail = async ({id}) => {
     </div>
   )
 }
+*/
+
+/*import React from 'react';
+import Image from 'next/image';
+import QuantSelec from './QuantSelec';
+
+export const ProductDetail = async ({id}) => {
+  console.log('detail id', id);
+try{
+    const response = await fetch(`http://${process.env.VERCEL_URL}/api/producto/${id}`,{cache:"no-store"})
+   
+    if(!response.ok){
+      throw new Error
+    }
+    const item = await response.json()
+    console.log('item detail',item);
+    return item
+}catch(error){
+  console.error(error)
+}
+    
+  return (
+    <div className='max-w-4xl m-auto py-40 justify-center items-center'>
+        <section>
+           <Image
+            alt={item.title}
+            src={`/products-img/${item.image}`}
+            width={450}
+            height={450}
+            style={{objectFit: "contain"}}/>
+            <h2>{item.title}</h2>
+            <h2>$ {item.price}</h2>
+            <h2>Descripcion: {item.description}</h2>
+            <h2>Categoria: {item.category}</h2>
+            <h2>Stock: {item.stock}</h2>
+            <QuantSelec item={item}/>
+        </section>   
+    </div>
+  )
+}
+*/
--- app/components/products/ProductList.jsx
+++ app/components/products/ProductList.jsx
@@ -3,7 +3,7 @@ import { ProductCard } from './ProductCard.jsx';
 
 
 export const ProductList = async ({ categoria }) => {
-    const items = await fetch(`http://${process.env.VERCEL_URL}/api/productos/${categoria}`,{cache:"no-store"}).then(r => r.json())
+    const items = await fetch(`http://${process.env.VERCEL_URL}/api/productos/${categoria}`,{cache:"no-store"}).then(r => r.json()).catch(Error)
     console.log('items',items);
 
     return (
@@ -14,3 +14,47 @@ export const ProductList = async ({ categoria }) => {
         </section>
     );
 }
+
+/*import React from 'react';
+import { ProductCard } from './ProductCard.jsx';
+
+
+export const ProductList = async ({ categoria }) => {
+    const items = await fetch(`http://${process.env.VERCEL_URL}/api/productos/${categoria}`,{cache:"no-store"}).then(r => r.json())
+    console.log('items',items);
+
+    return (
+        <section className='container m-auto'>
+             <div className='flex flex-wrap justify-center gap-10 pt-40'>
+            {items.map(item => <ProductCard key={item.id} item={item}/>)}
+            </div>
+        </section>
+    );
+}*/
+
+/*import React from 'react';
+import { ProductCard } from './ProductCard.jsx';
+
+
+export const ProductList = async ({ categoria }) => {
+    try{
+        const response = await fetch(`http://${process.env.VERCEL_URL}/api/productos/${categoria}`,{cache:"no-store"})
+        if(!response.ok){
+            throw new Error
+        }
+        const items = await response.json()
+        console.log('items', items);
+        return items
+    }catch(error){
+        console.error(error)
+    }
+  
+
+    return (
+        <section className='container m-auto'>
+             <div className='flex flex-wrap justify-center gap-10 pt-40'>
+            {items.map(item => <ProductCard key={item.id} item={item}/>)}
+            </div>
+        </section>
+    );
+}*/
