78c4ec97864ae71bd15c826330481135eb6ebd6b
blastonyz <blastonyzamora@gmail.com>
2024-05-27 21:22:27 -0300

    deploy16

--- app/components/ui/Counter.jsx
+++ app/components/ui/Counter.jsx
@@ -25,27 +25,6 @@ export const Counter = ({ counter, setCounter }) => {
   );
 };
 
-//import { useState } from 'react';
-import  Boton  from './Boton';
 
-const Counter = ({ counter, setCounter }) => {
-  console.log('counter', counter);
-
-  const increase = () => {
-      setCounter(counter + 1);
-  };
-
-  const decrease = () => {
-      setCounter(counter - 1);
-  };
-
-  return (
-      <div className='flex justify-center items-center gap-3'>
-          <Boton onClick={decrease}>-</Boton>
-          <p>{counter}</p>
-          <Boton onClick={increase}>+</Boton>
-      </div>
-  );
-};
 
 export default Counter;
