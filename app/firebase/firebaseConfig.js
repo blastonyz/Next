ff0015966a942cc7dc63cd4404f4a6a854167a23
blastonyz <blastonyzamora@gmail.com>
2024-08-06 10:18:58 -0300

    api-key problem

--- app/firebase/firebaseConfig.js
+++ app/firebase/firebaseConfig.js
@@ -4,14 +4,15 @@ import { getAuth } from "firebase/auth";
 import {getStorage} from "firebase/storage"
 
 
-export const firebaseConfig = {
-  apiKey:"next-cc43a.firebaseapp.com",
-  authDomain: "AIzaSyC8ke4C5odvQZfISAfwlRi_1sUSxUhZrSo",
-  projectId:"next-cc43a",
-  storageBucket: "next-cc43a.appspot.com",
-  messagingSenderId: "454727340926",
-  appId: "1:454727340926:web:14ae9aacd34f308ce6192f"
-};
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_SENDER_MESAGGING_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
+};;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
